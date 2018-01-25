b1d588a2497457db6c100298a76e7d4d13093961
aplanex <35802319+aplanex@users.noreply.github.com>
2018-01-25 14:52:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('C3Mdqz8rOa9aqEl9287KDeLjSKytsUjV'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
